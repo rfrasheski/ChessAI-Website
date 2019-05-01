5b67616c4ed525d51bbfaee4b302806539dae5a6
rfrasheski <rfrasheski@gmail.com>
2019-04-30 19:50:53 -0700

    Added project goals and library references to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,13 +19,34 @@ const IndexPage = () => {
           <h3>Ryan Frasheski + Shang Yang</h3>
           <h4>frashesk@seattleu.edu  |  yangs8@seattleu.edu</h4>
           <br/>
-          <Link to="">
+          {/*}<Link to="">
             <Button type="primary" size="large" style={{marginRight: 10}}>Play</Button>
-          </Link>
+          </Link>*/}
           <Button type="primary" size="large" href="https://github.com/rfrasheski/ChessAI">Github</Button>
         </div>
         <div align="center">
-
+        <br/>
+        <br/>
+          <h2>Project Goals</h2>
+          <h4>
+            The goal of this project is to create a chess playing AI to take on
+            the greatest chess grandmasters in the world. By incorporating the
+            topics learned in our AI course, we will bestow upon it the power to
+            crush its opponents mercilessly and effortlessly.
+            ChessAI will be made in Javascript and playable on this page.
+          </h4>
+          <br/>
+          <h2>Libraries and References</h2>
+          <a href="https://chessboardjsx.com/">Chessboardjsx - Web GUI</a>
+          <br/>
+          <a href="https://github.com/jhlywa/chess.js/blob/master/README.md">Chess.js - Game Logic</a>
+          <br/>
+          <br/>
+          <h4>
+            These libraries will provide the gameplay framework and GUI components that
+            our own AI systems can use to interact with players and understand
+            game state.
+          </h4>
         </div>
       </div>
     </Layout>
