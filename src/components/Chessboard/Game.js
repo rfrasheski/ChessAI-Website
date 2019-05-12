7af8841bf6eba4c8468f1268378d78c8dc87b84c
rfrasheski <rfrasheski@gmail.com>
2019-05-11 18:53:14 -0700

    Integrate chess.js and legal move highlighting

--- src/components/Chessboard/Game.js
+++ src/components/Chessboard/Game.js
@@ -1,256 +1,217 @@
-// import React, { Component } from "react";
-// import PropTypes from "prop-types";
-// //import Chess from 'chess.js'; // import Chess from  "chess.js"(default) if recieving an error about new Chess not being a constructor
-// import { Chess } from "chess.js"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+//import Chess from 'chess.js'; // import Chess from  "chess.js"(default) if recieving an error about new Chess not being a constructor
+import Chess from  "chess.js"
+import Chessboard from 'chessboardjsx'
 
-// const STOCKFISH = window.STOCKFISH;
-// var game = new Chess.Chess();
+class Game extends Component {
+  static propTypes = { children: PropTypes.func };
 
-// class Game extends Component {
-//   static propTypes = { children: PropTypes.func };
+  state = {
+    fen: "start",
+    // square styles for active drop square
+    dropSquareStyle: {},
+    // custom square styles
+    squareStyles: {},
+    // square with the currently clicked piece
+    pieceSquare: "",
+    // currently clicked square
+    square: "",
+    // array of past game moves
+    history: []
+  };
 
-//   state = { fen: "start" };
+  componentDidMount() {
+    this.game = new Chess();
+  }
 
-//   componentDidMount() {
-//     this.setState({ fen: game.fen() });
+  // keep clicked square style and remove hint squares
+  removeHighlightSquare = () => {
+    this.setState(({ pieceSquare, history }) => ({
+      squareStyles: squareStyling({ pieceSquare, history })
+    }));
+  };
 
-//     this.engineGame().prepareMove();
-//   }
+  // show possible moves
+  highlightSquare = (sourceSquare, squaresToHighlight) => {
+    const highlightStyles = [sourceSquare, ...squaresToHighlight].reduce(
+      (a, c) => {
+        return {
+          ...a,
+          ...{
+            [c]: {
+              background:
+                "radial-gradient(circle, #fffc00 36%, transparent 40%)",
+              borderRadius: "50%"
+            }
+          },
+          ...squareStyling({
+            history: this.state.history,
+            pieceSquare: this.state.pieceSquare
+          })
+        };
+      },
+      {}
+    );
 
-//   onDrop = ({ sourceSquare, targetSquare }) => {
-//     // see if the move is legal
-//     const move = game.move({
-//       from: sourceSquare,
-//       to: targetSquare,
-//       promotion: "q"
-//     });
+    this.setState(({ squareStyles }) => ({
+      squareStyles: { ...squareStyles, ...highlightStyles }
+    }));
+  };
 
-//     // illegal move
-//     if (move === null) return;
+  onDrop = ({ sourceSquare, targetSquare }) => {
+    // see if the move is legal
+    let move = this.game.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q" // always promote to a queen for example simplicity
+    });
 
-//     return new Promise(resolve => {
-//       this.setState({ fen: game.fen() });
-//       resolve();
-//     }).then(() => this.engineGame().prepareMove());
-//   };
+    // illegal move
+    if (move === null) return;
+    this.setState(({ history, pieceSquare }) => ({
+      fen: this.game.fen(),
+      history: this.game.history({ verbose: true }),
+      squareStyles: squareStyling({ pieceSquare, history })
+    }));
+  };
 
-//   engineGame = options => {
-//     options = options || {};
+  onMouseOverSquare = square => {
+    // get list of possible moves for this square
+    let moves = this.game.moves({
+      square: square,
+      verbose: true
+    });
 
-//     /// We can load Stockfish via Web Workers or via STOCKFISH() if loaded from a <script> tag.
-//     let engine =
-//       typeof STOCKFISH === "function"
-//         ? STOCKFISH()
-//         : new Worker(options.stockfishjs || "stockfish.js");
-//     let evaler =
-//       typeof STOCKFISH === "function"
-//         ? STOCKFISH()
-//         : new Worker(options.stockfishjs || "stockfish.js");
-//     let engineStatus = {};
-//     let time = { wtime: 3000, btime: 3000, winc: 1500, binc: 1500 };
-//     let playerColor = "black";
-//     let clockTimeoutID = null;
-//     // let isEngineRunning = false;
-//     let announced_game_over;
-//     // do not pick up pieces if the game is over
-//     // only pick up pieces for White
+    // exit if there are no moves available for this square
+    if (moves.length === 0) return;
 
-//     setInterval(function() {
-//       if (announced_game_over) {
-//         return;
-//       }
+    let squaresToHighlight = [];
+    for (var i = 0; i < moves.length; i++) {
+      squaresToHighlight.push(moves[i].to);
+    }
 
-//       if (game.game_over()) {
-//         announced_game_over = true;
-//       }
-//     }, 500);
+    this.highlightSquare(square, squaresToHighlight);
+  };
 
-//     function uciCmd(cmd, which) {
-//       // console.log('UCI: ' + cmd);
+  onMouseOutSquare = square => this.removeHighlightSquare(square);
 
-//       (which || engine).postMessage(cmd);
-//     }
-//     uciCmd("uci");
+  // central squares get diff dropSquareStyles
+  onDragOverSquare = square => {
+    this.setState({
+      dropSquareStyle:
+        square === "e4" || square === "d4" || square === "e5" || square === "d5"
+          ? { backgroundColor: "cornFlowerBlue" }
+          : { boxShadow: "inset 0 0 1px 4px rgb(255, 255, 0)" }
+    });
+  };
 
-//     function clockTick() {
-//       let t =
-//         (time.clockColor === "white" ? time.wtime : time.btime) +
-//         time.startTime -
-//         Date.now();
-//       let timeToNextSecond = (t % 1000) + 1;
-//       clockTimeoutID = setTimeout(clockTick, timeToNextSecond);
-//     }
+  onSquareClick = square => {
+    this.setState(({ history }) => ({
+      squareStyles: squareStyling({ pieceSquare: square, history }),
+      pieceSquare: square
+    }));
 
-//     function stopClock() {
-//       if (clockTimeoutID !== null) {
-//         clearTimeout(clockTimeoutID);
-//         clockTimeoutID = null;
-//       }
-//       if (time.startTime > 0) {
-//         let elapsed = Date.now() - time.startTime;
-//         time.startTime = null;
-//         if (time.clockColor === "white") {
-//           time.wtime = Math.max(0, time.wtime - elapsed);
-//         } else {
-//           time.btime = Math.max(0, time.btime - elapsed);
-//         }
-//       }
-//     }
+    let move = this.game.move({
+      from: this.state.pieceSquare,
+      to: square,
+      promotion: "q" // always promote to a queen for example simplicity
+    });
 
-//     function startClock() {
-//       if (game.turn() === "w") {
-//         time.wtime += time.winc;
-//         time.clockColor = "white";
-//       } else {
-//         time.btime += time.binc;
-//         time.clockColor = "black";
-//       }
-//       time.startTime = Date.now();
-//       clockTick();
-//     }
+    // illegal move
+    if (move === null) return;
 
-//     function get_moves() {
-//       let moves = "";
-//       let history = game.history({ verbose: true });
+    this.setState({
+      fen: this.game.fen(),
+      history: this.game.history({ verbose: true }),
+      pieceSquare: ""
+    });
+  };
 
-//       for (let i = 0; i < history.length; ++i) {
-//         let move = history[i];
-//         moves +=
-//           " " + move.from + move.to + (move.promotion ? move.promotion : "");
-//       }
+  onSquareRightClick = square =>
+    this.setState({
+      squareStyles: { [square]: { backgroundColor: "deepPink" } }
+    });
 
-//       return moves;
-//     }
+  render() {
+    const { fen, dropSquareStyle, squareStyles } = this.state;
 
-//     const prepareMove = () => {
-//       stopClock();
-//       // this.setState({ fen: game.fen() });
-//       let turn = game.turn() === "w" ? "white" : "black";
-//       if (!game.game_over()) {
-//         // if (turn === playerColor) {
-//         if (turn !== playerColor) {
-//           // playerColor = playerColor === 'white' ? 'black' : 'white';
-//           uciCmd("position startpos moves" + get_moves());
-//           uciCmd("position startpos moves" + get_moves(), evaler);
-//           uciCmd("eval", evaler);
+    return this.props.children({
+      squareStyles,
+      position: fen,
+      onMouseOverSquare: this.onMouseOverSquare,
+      onMouseOutSquare: this.onMouseOutSquare,
+      onDrop: this.onDrop,
+      dropSquareStyle,
+      onDragOverSquare: this.onDragOverSquare,
+      onSquareClick: this.onSquareClick,
+      onSquareRightClick: this.onSquareRightClick
+    });
+  }
+}
 
-//           if (time && time.wtime) {
-//             uciCmd(
-//               "go " +
-//                 (time.depth ? "depth " + time.depth : "") +
-//                 " wtime " +
-//                 time.wtime +
-//                 " winc " +
-//                 time.winc +
-//                 " btime " +
-//                 time.btime +
-//                 " binc " +
-//                 time.binc
-//             );
-//           } else {
-//             uciCmd("go " + (time.depth ? "depth " + time.depth : ""));
-//           }
-//           // isEngineRunning = true;
-//         }
-//         if (game.history().length >= 2 && !time.depth && !time.nodes) {
-//           startClock();
-//         }
-//       }
-//     };
+export default function WithMoveValidation() {
+  return (
+    <div style={boardsContainer}>
+      <Game>
+      {({
+          position,
+          onDrop,
+          onMouseOverSquare,
+          onMouseOutSquare,
+          squareStyles,
+          dropSquareStyle,
+          onDragOverSquare,
+          onSquareClick,
+          onSquareRightClick
+        }) => (
+          <Chessboard
+            id="Game"
+            width={500}
+            position={position}
+            onDrop={onDrop}
+            onMouseOverSquare={onMouseOverSquare}
+            onMouseOutSquare={onMouseOutSquare}
+            boardStyle={{
+              borderRadius: "5px",
+              boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
+            }}
+            squareStyles={squareStyles}
+            dropSquareStyle={dropSquareStyle}
+            onDragOverSquare={onDragOverSquare}
+            onSquareClick={onSquareClick}
+            onSquareRightClick={onSquareRightClick}
+          />
+        )}
+      </Game>
+    </div>
+  )
+};
 
-//     evaler.onmessage = function(event) {
-//       let line;
+const squareStyling = ({ pieceSquare, history }) => {
+  const sourceSquare = history.length && history[history.length - 1].from;
+  const targetSquare = history.length && history[history.length - 1].to;
 
-//       if (event && typeof event === "object") {
-//         line = event.data;
-//       } else {
-//         line = event;
-//       }
+  return {
+    [pieceSquare]: { backgroundColor: "rgba(255, 255, 0, 0.4)" },
+    ...(history.length && {
+      [sourceSquare]: {
+        backgroundColor: "rgba(255, 255, 0, 0.4)"
+      }
+    }),
+    ...(history.length && {
+      [targetSquare]: {
+        backgroundColor: "rgba(255, 255, 0, 0.4)"
+      }
+    })
+  };
+};
 
-//       // console.log('evaler: ' + line);
-
-//       /// Ignore some output.
-//       if (
-//         line === "uciok" ||
-//         line === "readyok" ||
-//         line.substr(0, 11) === "option name"
-//       ) {
-//         return;
-//       }
-//     };
-
-//     engine.onmessage = event => {
-//       let line;
-
-//       if (event && typeof event === "object") {
-//         line = event.data;
-//       } else {
-//         line = event;
-//       }
-//       // console.log('Reply: ' + line);
-//       if (line === "uciok") {
-//         engineStatus.engineLoaded = true;
-//       } else if (line === "readyok") {
-//         engineStatus.engineReady = true;
-//       } else {
-//         let match = line.match(/^bestmove ([a-h][1-8])([a-h][1-8])([qrbn])?/);
-//         /// Did the AI move?
-//         if (match) {
-//           // isEngineRunning = false;
-//           game.move({ from: match[1], to: match[2], promotion: match[3] });
-//           this.setState({ fen: game.fen() });
-//           prepareMove();
-//           uciCmd("eval", evaler);
-//           //uciCmd("eval");
-//           /// Is it sending feedback?
-//         } else if (
-//           (match = line.match(/^info .*\bdepth (\d+) .*\bnps (\d+)/))
-//         ) {
-//           engineStatus.search = "Depth: " + match[1] + " Nps: " + match[2];
-//         }
-
-//         /// Is it sending feed back with a score?
-//         if ((match = line.match(/^info .*\bscore (\w+) (-?\d+)/))) {
-//           let score = parseInt(match[2], 10) * (game.turn() === "w" ? 1 : -1);
-//           /// Is it measuring in centipawns?
-//           if (match[1] === "cp") {
-//             engineStatus.score = (score / 100.0).toFixed(2);
-//             /// Did it find a mate?
-//           } else if (match[1] === "mate") {
-//             engineStatus.score = "Mate in " + Math.abs(score);
-//           }
-
-//           /// Is the score bounded?
-//           if ((match = line.match(/\b(upper|lower)bound\b/))) {
-//             engineStatus.score =
-//               ((match[1] === "upper") === (game.turn() === "w")
-//                 ? "<= "
-//                 : ">= ") + engineStatus.score;
-//           }
-//         }
-//       }
-//       // displayStatus();
-//     };
-
-//     return {
-//       start: function() {
-//         uciCmd("ucinewgame");
-//         uciCmd("isready");
-//         engineStatus.engineReady = false;
-//         engineStatus.search = null;
-//         prepareMove();
-//         announced_game_over = false;
-//       },
-//       prepareMove: function() {
-//         prepareMove();
-//       }
-//     };
-//   };
-
-//   render() {
-//     const { fen } = this.state;
-//     return this.props.children({ position: fen, onDrop: this.onDrop });
-//   }
-// }
-
-// export default Game;
+const boardsContainer = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexWrap: "wrap",
+  marginTop: 30,
+  marginBottom: 50
+};
--- src/components/Chessboard/index.js
+++ src/components/Chessboard/index.js
@@ -1,5 +1,3 @@
-import Board from './Board'
 import Game from './Game'
-import { Chess } from 'chess.js'
 
-export default Board
+export default Game
